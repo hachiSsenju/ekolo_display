bf145b1c5310a9626d914135f3eec461c6608ae2
aihorizonconsulting <aihorizonconsulting@gmail.com>
2026-04-01 14:59:45 +0100

    feat: initialize PostHog analytics and add .env to gitignore

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,13 @@
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
-  import "./i18n";
+import { createRoot } from "react-dom/client";
+import posthog from "posthog-js";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import "./i18n";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+posthog.init("phc_CeNznVb73CpaFWVCBnLQ5xuBesVuipyXikSn3SdrjtcY", {
+  api_host: "https://us.i.posthog.com",
+  capture_pageview: true,
+  capture_pageleave: true,
+});
+
+createRoot(document.getElementById("root")!).render(<App />);
